4cd39cfc2df416176982b7df97b0b95108de4f22
Stefan Bunciak <pipistik.bunciak@gmail.com>
2015-03-06 14:25:56 +0100

    Update examples to use current silverspoon deps

--- src/main/java/org/bulldog/examples/DigitalOutputExample.java
+++ src/main/java/org/bulldog/examples/DigitalOutputExample.java
@@ -1,58 +1,55 @@
 package org.bulldog.examples;
 
-import java.io.IOException;
+import io.silverspoon.bulldog.beagleboneblack.BBBNames;
+import io.silverspoon.bulldog.core.Signal;
+import io.silverspoon.bulldog.core.gpio.DigitalOutput;
+import io.silverspoon.bulldog.core.platform.Board;
+import io.silverspoon.bulldog.core.platform.Platform;
+import io.silverspoon.bulldog.core.util.BulldogUtil;
 
-import org.bulldog.beagleboneblack.BBBNames;
-import org.bulldog.core.Signal;
-import org.bulldog.core.gpio.DigitalOutput;
-import org.bulldog.core.platform.Board;
-import org.bulldog.core.platform.Platform;
-import org.bulldog.core.util.BulldogUtil;
+import java.io.IOException;
 
 public class DigitalOutputExample {
 
-    public static void main(String[] args) throws IOException
-    {
-    	//Detect the board we are running on
-    	Board board = Platform.createBoard();
-    	
-    	//Set up a digital output
-    	DigitalOutput output = board.getPin(BBBNames.P8_12).as(DigitalOutput.class);
-    	
-    	//Set a high signal on the output
-    	output.write(Signal.High);
-    	
-    	//Toggle it
-    	output.toggle();
-    	BulldogUtil.sleepMs(500);
-    	
-    	//Set it high
-    	output.high();
-    	BulldogUtil.sleepMs(500);
-    	
-    	//Set it low
-    	output.low();
-    	BulldogUtil.sleepMs(500);
-    	
-    	//Yet another way of applying a signal
-    	output.applySignal(Signal.High);
-    	BulldogUtil.sleepMs(500);
-    	
-    	//Let it blink 3 times
-    	output.blinkTimes(500, 3);
-    	output.awaitBlinkingStopped();
-    	
-    	//Let it blink 5 seconds, 10 times a second
-    	output.startBlinking(100, 5000);
-    	output.awaitBlinkingStopped();
-    	
-    	//Let it blink 5 times a second indefinitely
-    	output.startBlinking(200);
-    	
-    	while(true) {
-    		BulldogUtil.sleepMs(50);
-    	}
-  
-    }
-	
+   public static void main(String[] args) throws IOException {
+      // Detect the board we are running on
+      Board board = Platform.createBoard();
+
+      // Set up a digital output
+      DigitalOutput output = board.getPin(BBBNames.P8_12).as(DigitalOutput.class);
+
+      // Set a high signal on the output
+      output.write(Signal.High);
+
+      // Toggle it
+      output.toggle();
+      BulldogUtil.sleepMs(500);
+
+      // Set it high
+      output.high();
+      BulldogUtil.sleepMs(500);
+
+      // Set it low
+      output.low();
+      BulldogUtil.sleepMs(500);
+
+      // Yet another way of applying a signal
+      output.applySignal(Signal.High);
+      BulldogUtil.sleepMs(500);
+
+      // Let it blink 3 times
+      output.blinkTimes(500, 3);
+      output.awaitBlinkingStopped();
+
+      // Let it blink 5 seconds, 10 times a second
+      output.startBlinking(100, 5000);
+      output.awaitBlinkingStopped();
+
+      // Let it blink 5 times a second indefinitely
+      output.startBlinking(200);
+
+      while (true) {
+         BulldogUtil.sleepMs(50);
+      }
+   }
 }
